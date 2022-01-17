787b08547b023152ff8f49062cb55dca91da10c3
afbolivarg <afbolivarg@gmail.com>
2022-01-17 11:06:47 -0500

    Author credit

--- src/TodoWizardHeader/index.js
+++ src/TodoWizardHeader/index.js
@@ -4,12 +4,15 @@ import './TodoWizardHeader.css'
 function TodoWizardHeader() {
   return (
     <div className='header'>
-      <img
-        className='logo'
-        src='https://img.icons8.com/glyph-neue/64/39A6A3/fantasy.png'
-        alt='Logo'
-      />
-      <h1 className='title'>ToDo Wizard</h1>
+      <div className='big-title'>
+        <img
+          className='logo'
+          src='https://img.icons8.com/glyph-neue/64/39A6A3/fantasy.png'
+          alt='Logo'
+          />
+        <h1 className='title'>ToDo Wizard</h1>
+      </div>
+        <h3 className='author'>Coded by <a href='https://github.com/afbolivarg' target='blank'>Andrés Bolívar</a></h3>
     </div>
   );
 }
